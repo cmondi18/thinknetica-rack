d1c28d54e6d06dd893bda6b3303d0c66e779381f
cmondi18 <disivolap@gmail.com>
2022-03-26 00:00:28 +0300

    feat: add time rake app

---
+++ app.rb
@@ -0,0 +1,55 @@
+class TimeApp
+  @@status = 404
+  @@headers = { 'Content-Type' => 'text/plain' }
+  @@body = ["Not Found"]
+
+  @@permit_formats = %w[year month day hour minute second]
+
+  def call(env)
+    req = Rack::Request.new(env)
+    @@has_unknown = false
+
+    if req.path == '/time'
+      if req.params.key?('format')
+        formats = req.params['format'].split(',')
+        check_unknown(formats)
+        set_format(formats) unless @@has_unknown
+      end
+    else
+      @@status = 404
+      @@headers = { 'Content-Type' => 'text/plain' }
+      @@body = ["Not Found"]
+    end
+    [@@status, @@headers, @@body]
+  end
+
+  private
+
+  def set_format(formats)
+    time = []
+    formats.each do |format|
+      if format == 'minute'
+        time.append(Time.now.send('min').to_s)
+      elsif format == 'second'
+        time.append(Time.now.send('sec').to_s)
+      else
+        time.append(Time.now.send(format).to_s)
+      end
+      @@body = ["#{time.join('-')}"]
+    end
+  end
+
+  def check_unknown(formats)
+    unknown_formats = []
+    formats.each do |format|
+      unless @@permit_formats.include?(format)
+        unknown_formats.append(format)
+      end
+    end
+    if unknown_formats.any?
+      @@has_unknown = true
+      @@status = 400
+      @@body = ["Unknown time format #{unknown_formats}"]
+    end
+  end
+end
